df2fc79b0e3240dc7d51dbdcb3ef2d886411c109
Juddy4134 <Kore984134@gmail.com>
2026-05-12 11:46:59 +0300

    added PWA settings

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "shift-calendar-v1";
+
+const urls = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(urls))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
